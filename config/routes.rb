453b50a11979e999af12a96ed0c02eb7a7e6df52
rodrigo-morais <morais.rm@gmail.com>
2018-03-25 15:51:06 +0200

    Adding health check to API.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   defaults format: :json do
     resources :transactions, only: [:index, :show]
+    resources :health, only: [:index]
   end
 end
